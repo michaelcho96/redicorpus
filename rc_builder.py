d5bd9c4486282bea963bb5d5d11425717557856b
Dillon Niederhut <dillon.niederhut@gmail.com>
2014-08-29 00:38:57 -0700

    Finished corpus builder

--- rc_builder.py
+++ rc_builder.py
@@ -7,39 +7,68 @@ __author__ = "Dillon Niederhut"
 __version__ = "0.0.1"
 __email__ = "dillon.niederhut@gmail.com"
 
-import requests, re, io, time, os
+import requests, re, io, time, os, glob
 from lxml import etree
- 
-page = requests.get('http://reddit.com/r/Askreddit/.xml', headers = {
-    'User-Agent' : 'redicorpus v. ' + __version__,
-    'From' : __email__
-}).content
+from nltk import util, word_tokenize, PorterStemmer
 
-links = list()
-for element in page.iter('guid'):
-    links.append(element.text)
+def get_links():
+    page = etree.XML(requests.get('http://reddit.com/r/Askreddit/.xml', headers = {
+        'User-Agent' : 'redicorpus v. ' + __version__,
+        'From' : __email__
+    }).content)
+    links = list()
+    for element in page.iter('guid'):
+        links.append(element.text)
+    return links
 
-try:
-    os.makedirs(os.getcwd() + "/redicorpus/pages/" + time.strftime("%Y_%m_%d"))
-except OSError:
-    if not os.path.isdir(os.getcwd() + "/redicorpus/pages/" + time.strftime("%Y_%m_%d")):
-        raise
-os.chdir(os.getcwd() + "/redicorpus/pages/" + time.strftime("%Y_%m_%d"))
+def get_pages():
+    links = get_links()
+    rcdir = os.getcwd()
+    now = time.strftime("%Y_%m_%d")
+    try:
+        os.makedirs(rcdir + "/pages/" + now)
+    except OSError:
+        if not os.path.isdir(rcdir + "/pages/" + now):
+            raise
+    os.chdir(rcdir + "/pages/" + now)
+    for i in links:
+        url = str(i+".xml/?limit=500/?depth=100")
+        page = requests.get(url,headers = {
+        'User-Agent' : 'redicorpus v. ' + __version__,
+        'From' : __email__}).content
+        name = str(re.search(r'[^http://www.reddit.com/r/AskReddit/comments/].{5,5}', i).group() + '.xml')
+        f = open(name, 'w')
+        f.write(page)##error: write function is inserting html tag at beginning of file
+        f.close()
+        time.sleep(2)
+    return rcdir, now
 
-for i in links:
-    url = str(i+".json/?limit=500/?depth=100")
-    page = requests.get(url,headers = headers).content
-    name = print re.search(r'[^http://www.reddit.com/r/AskReddit/comments/].{5,5}', i).group() + ".txt"
-    with io.open(name, 'w', encoding = 'utf-8') as f:
-        f.write(unicode(json.dumps(page, esure_ascii = False)))
-    time.sleep(2)
+def build_corpus():
+    rcdir, now = get_pages()
+    comments = unicode()
+    for filename in glob.glob('*.xml'):
+        root = etree.XML(filename)
+        for element in root.iter('description'):
+            comments.join(element.text)
+    try:
+        os.makedirs(rcdir + "/corpora/" + now)
+    except OSError:
+        if not os.path.isdir(rcdir + "/corpora/" + now):
+            raise
+    os.chdir(os.getcwd() + "/corpora/" + now)
+    stems = [PorterStemmer().stem(t) for t in word_tokenize(comments.lower())]
+    f = open('unigram.txt', 'w')
+    f.write(sorted(util.ngrams(stems, 1)))
+    f.close
+    f = open('bigram.txt', 'w')
+    f.write(sorted(util.ngrams(stems, 2)))
+    f.close    
+    f = open('trigram.txt', 'w')
+    f.write(sorted(util.ngrams(stems, 3)))
+    f.close
+    return "Done!"
 
-page = requests.get('http://www.reddit.com/r/AskReddit/comments/2dzc2f/what_is_a_completely_rational_sentence_you_could/.json', headers = {
-    'User-Agent' : 'redicorpus v. ' + __version__,
-    'From' : __email__
-})
-
-f = open('Example.json', 'w')
-f.write(page.content)
-f.close()
-
+if __name__ == "__main__":
+    build_corpus()
+else:
+    print "Goodbye"
