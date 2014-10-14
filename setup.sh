dfba3e2dddb0aa15f9c1891411e65eaae856618e
Dillon Niederhut <dillon.niederhut@gmail.com>
2014-10-14 11:16:30 -0700

    Added .bashrc edit to setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-## Configuration script for redicorpus
 
+# Configuration script for redicorpus
+# Takes directory as an input
+# Copies redicorpus folder into directory
+# Appends export RCDIR to bashrc in home directory
+# Appends crontab command to run rcbuilder at 8pm local time
+
+read -e -p "Enter directory for redicorpus" -i "~/redicorpus" RCDIR
+export RCDIR
+echo 'export RCDIR' >> ~/.bashrc
+mkdir -m 777 RCDIR
 folder=$(pwd)
-mkdir -m 777 ~/redicorpus/
-cp -r $folder/ ~/redicorpus/
-## Need to add in command to append 'EXPORT RCDIR=~/redicorpus/
+cp -r $folder/ RCDIR
 ( crontab -l | grep -v "python ~/redicorpus/rc_builder.py" ; echo "0 20 * * * python ~/redicorpus/rc_builder.py" ) | crontab -
