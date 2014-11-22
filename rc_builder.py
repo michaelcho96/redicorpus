58ea12e534f7e0bc48b183f8a9055014dbff527a
Dillon Niederhut <dillon.niederhut@gmail.com>
2014-11-22 15:35:05 -0800

    Changes to comment cleaner; expected calculations are now chisq

--- rc_builder.py
+++ rc_builder.py
@@ -7,7 +7,13 @@ __author__ = "Dillon Niederhut"
 __version__ = "0.0.2"
 __email__ = "dillon.niederhut@gmail.com"
 
-import requests, re, time, os, glob, logging
+import requests 
+import re
+import time
+import os
+import glob
+import logging
+
 RCDIR = os.environ.get('RCDIR')
 NOW = time.strftime("%Y_%m_%d")
 logging.basicConfig(filename = RCDIR + '/rc_builder.log', level = logging.INFO, format = '%(asctime)s %(message)s')
@@ -75,8 +81,9 @@ def build_corpus(directory = RCDIR, date = NOW):
     logging.info('Comment number = ' + str(len(comments)))
     comments = ' '.join(comments)
     comments = comments.encode('ascii','ignore')
-    for i in (',','.',':',';','"'):
+    for i in (',','.',':',';','"','*',"'",'~','|','!','quot','<','>'):
         comments = comments.replace(i,'')
+    comments = comments.replace('&','and')
     try:
         os.makedirs(RCDIR + "/corpora/" + date)
     except OSError:
@@ -91,7 +98,7 @@ def build_corpus(directory = RCDIR, date = NOW):
         logging.info(str(i) + 'gram number = ' + str(len(body)))
         dictionary = dict()
         while len(body) > 0:
-            element = body.pop()
+            element = ' '.join(body.pop())
             if dictionary.has_key(element):
                 dictionary.update({element:dictionary.get(element) + 1})
             else:
@@ -103,31 +110,44 @@ def build_corpus(directory = RCDIR, date = NOW):
 
 def dailies(directory = RCDIR, date = NOW):
     # Compares a day's corpus with previous days' corpora (at least one week
-    # and up to one month), finds the top ten most unique unigrams, bigrams,
-    # and trigrams, and outputs them as a dated file
+    # and up to one month), finds the top twenty-five most unique (as measured
+    # by tf-idf and multiples of expexted frequency) unigrams, bigrams, and
+    # trigrams as, and outputs them as dated files
     import math
     from ast import literal_eval
     if len(glob.glob(RCDIR + '/corpora/*')) > 7:
-        logging.info('Running top ten tf-idf ranking')
+        logging.info('Running dailies')
         top_tfidf = list()
+        top_expected = list()
+        top_google = list()
         for i in (1,2,3):
             corpus = list()
             tfidf_coef = list()
+            expected_coef = list()
             file_list = sorted(glob.glob(RCDIR + '/corpora/*/' + str(i) + 'gram.txt'))
             document_N = len(file_list[-31:])
+            term_N = int()
             with open(file_list[-1],'r') as f:
                 today = literal_eval(f.read())
             for filename in file_list[-31:]:
                 with open(filename, 'r') as f:
                     corpus.append(literal_eval(f.read()))
-            for key in today.iterkeys():
+            for document in corpus:
+                term_N += len(document)
+            for key in today.keys():
                 document_freq = float()
+                term_freq = float()
                 for document in corpus:
                     if document.has_key(key):
                         document_freq += 1
-                tfidf_coef.append((today.get(key)/math.log(document_N/document_freq),key))
-            for value_pair in sorted(tfidf_coef, reverse = True)[:10]:
-                top_tfidf.append(value_pair)
+                        term_freq += document.pop(key)
+                tfidf_coef.append((today.get(key) * math.log(document_N/document_freq),key))
+                expected_coef.append((today.get(key)*term_N / (len(today)*term_freq),key))
+                expected_coef.append((((today.get(key)-len(today)*term_freq/term_N)**2) / (len(today)*term_freq/term_N),key))
+            for coef, gram in sorted(tfidf_coef, reverse = True)[:25]:
+                top_tfidf.append((gram, coef))
+            for coef, gram in sorted(expected_coef, reverse = True)[:25]:
+                top_expected.append((gram, coef))          
         try:
             os.makedirs(RCDIR + "/dailies/tfidf/")
         except OSError:
@@ -135,8 +155,17 @@ def dailies(directory = RCDIR, date = NOW):
                 raise
         os.chdir(RCDIR + "/dailies/tfidf/")
         with open(date + '.txt','w') as f:
-            f.write(str(top_ten))
+            f.write(str(top_tfidf))
         logging.info(date + '.txt' + 'saved in ' + RCDIR + "/dailies/tfidf/")
+        try:
+            os.makedirs(RCDIR + "/dailies/expected/")
+        except OSError:
+            if not os.path.isdir(RCDIR + "/dailies/expected/"):
+                raise
+        os.chdir(RCDIR + "/dailies/expected/")
+        with open(date + '.txt','w') as f:
+            f.write(str(top_expected))
+        logging.info(date + '.txt' + 'saved in ' + RCDIR + "/dailies/expected/")
     else:
         logging.info('Not enough data to run comparison')
 
