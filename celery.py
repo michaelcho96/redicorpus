a7fca6c53f5bf13503fc6686758c4359fede9fd1
Dillon Niederhut <dillon.niederhut@gmail.com>
2015-10-04 03:17:16 -0700

    daemon replaced with celery

---
+++ celery.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+
+from celery import Celery
+
+app = Celery('redicorpus',
+             broker='mongodb://localhost:27017/celery',
+             include=['redicorpus.askreddit_getter','redicorpus.redicorpus'])
+
+# Optional configuration, see the application user guide.
+app.conf.update(
+    CELERY_TASK_RESULT_EXPIRES=3600,
+)
+
+if __name__ == '__main__':
+    app.start()
